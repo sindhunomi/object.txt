4b22c765d0a826a72a3b0b4d15baeb66df1f0c93
sindhunomi <sindhusrihari16@gmail.com>
2021-11-21 18:29:49 +0530

    performs various operations

---
+++ Add.java
@@ -0,0 +1,56 @@
+class Add
+{
+  public static void main(String[]args)    
+  {
+   int a = 10;
+   int b = 20;
+   int c = a+b;
+   int d = a*b;
+   int e = ++a;
+   int f = a--;
+   int g = --b;
+   System.out.println("sum of a and b is:" + c);
+   System.out.println("multiplication of a and b is: " + d);
+   System.out.println("increament of a:" + e);
+   System.out.println("decreament of a : " + f);
+   System.out.println("decreament of b :"+ g);
+   System.out.println("***operations performed***");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
