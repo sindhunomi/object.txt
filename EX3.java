dfbf25244ee65531853b8e8e4695b75dd8127238
sindhunomi <sindhusrihari16@gmail.com>
2021-11-24 19:09:32 +0530

    it is a java program

---
+++ EX3.java
@@ -0,0 +1,70 @@
+class LibraryAccount
+ {
+      int bookno;
+      String accountholder_name;
+      double amount; 
+      int book_renewal;
+     double payamount;
+     public double payamount()
+     {
+      return this.payamount;
+     }
+     public int book_renewal ()
+    {
+       return  this.book_renewal;
+    }
+        public LibraryAccount( int bookno, String accountholder_name, double amount )
+        {
+             this.bookno = bookno;
+             this.accountholder_name= accountholder_name;
+             this.amount= amount;
+        }
+}
+ public class EX3
+ {
+      public static void main (String args[])
+      { 
+         LibraryAccount la = new LibraryAccount( 72,"sindhu",500.0 );
+         System.out.println(la.bookno); 
+         System.out.println(la.accountholder_name);
+         System.out.println(la.amount);
+         la.payamount();
+     } 
+}      
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
