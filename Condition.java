b93a7cf5a2552029356c7b03c725f113b15d2cc7
sindhunomi <sindhusrihari16@gmail.com>
2021-11-25 08:00:37 +0530

    day 1 program

--- Condition.java
+++ Condition.java
@@ -1,15 +1,15 @@
 class Condition{
           public static void main(String args[]){
            int n = 7;
-             if (5%2==0 && n>=2  && n<=5)
+             if (7%2==0 && n>=2  && n<=5)
             {
              System.out.println("it is not weired ");
              }
-             else if(5%2==0 && n>=6 && n<=20)
+             else if(7%2==0 && n>=6 && n<=20)
            {
            System.out.println("it is weired ");
            }
-            else if(4%2==0 && n >20 )
+            else if(7%2==0 && n >20 )
           {
           System.out.println("it is not not weired");
          }
