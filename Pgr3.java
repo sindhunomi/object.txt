b05c8c78e3dbf9b56cb2d300fda265e73ae1e391
sindhunomi <sindhusrihari16@gmail.com>
2021-11-25 15:02:46 +0530

    day 1 program

--- Pgr3.java
+++ Pgr3.java
@@ -2,16 +2,14 @@ import java.util.Scanner;
 public class Pgr3{  
       public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter an  integer value" );
-        int num1= sc.nextInt();
-        System.out.println("enter a double value" ); 
-         Double num2=sc.nextDouble();
-         System.out.println("enter a string" );
-         String c = sc.next();
-      System.out.println(" integer : " + num1) ;
-      System.out.println(" double : " + num2);
-      System.out.println(" String : "  + c);
-      sc.close();
+        int a = sc.nextInt();
+        Double b =  sc.nextDouble();
+        String s = sc.nextLine();
+               s=sc.next();
+       System.out.println("String :"  +s );
+       System.out.println("double :" +b);
+       System.out.println("int :"  +a);
+        sc.close();
        }
    }
 
