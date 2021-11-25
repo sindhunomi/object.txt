b036d5a0fde23043fa1fb60faaaf1071cff45f6d
sindhunomi <sindhusrihari16@gmail.com>
2021-11-25 07:56:22 +0530

    day 1 program

---
+++ Condition.java
@@ -0,0 +1,22 @@
+class Condition{
+          public static void main(String args[]){
+           int n = 7;
+             if (5%2==0 && n>=2  && n<=5)
+            {
+             System.out.println("it is not weired ");
+             }
+             else if(5%2==0 && n>=6 && n<=20)
+           {
+           System.out.println("it is weired ");
+           }
+            else if(4%2==0 && n >20 )
+          {
+          System.out.println("it is not not weired");
+         }
+         else
+          {
+           System.out.println("print weired");
+         }
+      }
+}
+
