aefd53be2e236950ab6cb078a61801d9fb0ff2c0
sindhunomi <sindhusrihari16@gmail.com>
2021-11-25 11:51:47 +0530

    day 1 program

---
+++ Pgr3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Pgr3{  
+     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter an  integer value" );
+        int num1= sc.nextInt();
+        System.out.println("enter a double value" ); 
+         Double num2=sc.nextDouble();
+         System.out.println("enter a string" );
+         String c = sc.next();
+        sc.close();
+       }
+   }
+
