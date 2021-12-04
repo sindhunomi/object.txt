c781d858003937d1488f9411983df29f6856337e
sindhunomi <sindhusrihari16@gmail.com>
2021-12-04 18:22:12 +0530

    a program on type conversion

---
+++ Type.java
@@ -0,0 +1,10 @@
+   class Type
+     {
+         public static void main (String args[])
+           {
+                byte a =10; 
+                byte b =15;
+                byte sum =(byte)(a+b);
+               System.out.println("sum is : " + sum);
+             }
+     }
