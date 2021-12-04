80a285dcb8e3c8ee402d4244463d9cd827a5dd5c
sindhunomi <sindhusrihari16@gmail.com>
2021-12-04 18:53:41 +0530

    MAX MAIN VALUES

---
+++ Binary.java
@@ -0,0 +1,14 @@
+ class Binary
+  {
+      public static void main(String args[])
+           {
+            System.out.println(Integer.toBinaryString(127));
+            System.out.println(Integer.toHexString(13)); 
+             System.out.println(Integer.MAX_VALUE + " ") ;
+             System.out.println(Integer.MIN_VALUE + " " );
+             System.out.println(Byte.MAX_VALUE + " " );
+              System.out.println(Byte.MIN_VALUE + " ");
+              System.out.println(Short.MAX_VALUE + " ");
+              System.out.println(Short.MIN_VALUE + " ");
+             }
+    }
