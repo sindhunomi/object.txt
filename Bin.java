564a20ece2f79de612cc6af8362144707f185bc9
sindhunomi <sindhusrihari16@gmail.com>
2021-12-05 15:19:27 +0530

    binary value to integer

---
+++ Bin.java
@@ -0,0 +1,8 @@
+  class Bin
+    { 
+      public static void main(String args[])
+           {
+              Integer I = Integer.valueOf("1111", 2);
+              System.out.println(I);
+            }
+     }
