91b929f4064477c2ed6dc525259574fb72307062
sindhunomi <sindhusrihari16@gmail.com>
2021-12-05 15:11:11 +0530

    unary operators

---
+++ Unary.java
@@ -0,0 +1,17 @@
+  class Unary
+     {
+       public static void main (String args[])
+         {
+               int i2 = 1;
+              byte a = 5;
+             System.out.println(a++  +   ++a  + --a  +  a-- );
+             System.out.println(a<<1);
+             System.out.println(a>>1);
+              for(int i = 3; i<=10;) 
+                    {
+                           System.out.println("values are : " +  ++i);
+                      }    
+                           
+                          System.out.println("values2 are : "  +   ++i2);
+                 }
+           }
