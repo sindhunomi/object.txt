ccc08d880f0d0d3fb185bb78ad1b25c32207ba30
sindhunomi <sindhusrihari16@gmail.com>
2021-12-05 15:25:39 +0530

    some of the wrapper class methods

--- Binary.java
+++ Binary.java
@@ -1,8 +1,10 @@
  class Binary
   {
       public static void main(String args[])
-           {
-            System.out.println(Integer.toBinaryString(127));
+           {    
+               int a=500;
+               int b=1000;
+             System.out.println(Integer.toBinaryString(127));
             System.out.println(Integer.toHexString(13)); 
              System.out.println(Integer.MAX_VALUE + " ") ;
              System.out.println(Integer.MIN_VALUE + " " );
@@ -10,5 +12,7 @@
               System.out.println(Byte.MIN_VALUE + " ");
               System.out.println(Short.MAX_VALUE + " ");
               System.out.println(Short.MIN_VALUE + " ");
-             }
+             System.out.println(Integer.max(a,b));
+              System.out.println(Integer.min(a,b));
+            }
     }
