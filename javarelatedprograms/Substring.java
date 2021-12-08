df6f22d154e962a8c9c0b0c56ad2fef71aaf177a
sindhunomi <sindhusrihari16@gmail.com>
2021-12-08 12:02:35 +0530

    Day 4 program

--- javarelatedprograms/Substring.java
+++ javarelatedprograms/Substring.java
@@ -8,7 +8,11 @@ class Substring
       System.out.println(s);
       String str =s.substring(0);
       System.out.println(str); 
-      String str2 =s.substring(3,7);
-      System.out.println(str2);
-    }
-}
+      String str2 =s.substring(3,7);           
+      System.out.println(str2);                  //smaple input
+    }                                                           // Helloworld
+}                                                                
+                                                               //sample output
+                                                                   Helloworld
+                                                                   Helloworld
+                                                                     lowo
