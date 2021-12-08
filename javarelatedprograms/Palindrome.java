af9d7cd356bb95e4be0d1d6ef9c4c27df04ffbd2
sindhunomi <sindhusrihari16@gmail.com>
2021-12-08 13:30:35 +0530

    Day 4 program

---
+++ javarelatedprograms/Palindrome.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+class Palindrome
+  {
+    public static void main(String args[])
+        {
+               String a,b ="";
+               Scanner sc =new Scanner(System.in);
+                  a=sc.next();
+                int n=a.length();
+              for(int i=n-1;i>=0;i--)    
+                     {
+                         b=b+a.charAt(i);                    
+                     }
+                   if(a.equalsIgnoreCase(b))
+                    System.out.println("yes");
+                     else
+                    System.out.println("no");
+            }
+  }
