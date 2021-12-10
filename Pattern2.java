6f8c859fbea8d9c5db6c5002790ce4dc707e33c6
sindhunomi <sindhusrihari16@gmail.com>
2021-12-10 17:52:24 +0530

    patterns

---
+++ Pattern2.java
@@ -0,0 +1,24 @@
+import java.util.*;  
+class Pattern2
+   {
+      public static void main(String args[])
+         {
+            Scanner sc = new Scanner();
+              int c = sc.next();
+               for(int i=1;i<=n;i++)
+                {
+                   for(int j=1;j<=i;j++)
+                        {
+                        System.out.println("*");
+                        }  
+                     System.out.println();
+                  }
+                }
+       }                                                                                                    //sample input-->*
+                                                                                                              //sample output:
+                                                                                                               //*
+                                                                                                               //**
+                                                                                                               //***
+                                                                                                                //****
+                                                                                                                 //*****
+                                                                                                                                 
