c765ec9a764e5071a1da72a0b514d8ba0bcd02b5
sindhunomi <sindhusrihari16@gmail.com>
2021-12-22 18:33:37 +0530

    day 9 program

---
+++ javarelatedprograms/Patternd90.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Pattern90
+  {
+   public static void main (String args[])
+     {
+         Scanner sc = new Scanner(System.in);
+           int n = sc.nextInt();
+              for(int i =1;i<=n;i++)
+                {    
+                   for(int j=1;j<=i;j++ )
+                          {
+                              for( i=1;i<=n;i++)
+                                {
+                                   for(j=1;j<=i;j++)
+                                     {
+                                    System.out.print("*"+"");
+                                   }
+                              System.out.println();
+                               }
+                         }
+                     }
+                  }
+             }                                                            
+
