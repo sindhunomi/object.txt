84f6da42173a3b3dd5ce57712327ffa28e898833
sindhunomi <sindhusrihari16@gmail.com>
2021-12-22 18:32:37 +0530

    day  9 program

---
+++ javarelatedprograms/Patternd9.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Patternd9
+{
+   public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+             for(int i =1;i<=n-2;i++)
+                 {
+                     for(int j=1;j<=n;j++)
+                        {
+                            System.out.print(  "*"  +" ");
+                         }
+                            System.out.println();
+                     }
+          }
+}
+   //sample input --> 5
+
+   //sample output -->  * * * * *
+    		* * * * *
+    		* * * * *
+
