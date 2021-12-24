036cff7a2d045616b28a9c13323db73cff276029
sindhunomi <sindhusrihari16@gmail.com>
2021-12-24 16:46:28 +0530

    string methods

---
+++ javarelatedprograms/Tpoint2.java
@@ -0,0 +1,16 @@
+class Tpoint2
+ {
+   public static void main(String args[])
+    {
+        String str = "";
+       String str2 = " HARI";
+          if(str.isEmpty())
+             System.out.println("String " + str  + " is empty" );
+           else
+              System.out.println(str);
+           if(str2.isEmpty())
+                  System.out.println("String " + str2 + " is empty" );
+           else
+              System.out.println(str2);
+          }
+}
