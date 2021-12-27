71c7e42ab7bdf1bd60334093c48cc90063dab00d
sindhunomi <sindhusrihari16@gmail.com>
2021-12-27 12:25:43 +0530

    fibanocci series using for loop

---
+++ javarelatedprograms/Fibanocci2.java
@@ -0,0 +1,23 @@
+class Fibanocci2 {
+       public static void main(String args[]){
+           int f1=0, f2=1,f3=0,count =10;
+           System.out.println(f1+"\n"+f2);
+             for(int i=2;i<count;i++)
+                {
+                    f3 =f1+f2;
+                    f1 =f2;
+                    f2=f3;
+                 System.out.println(f3);
+                }
+            }
+        }   
+            
+           
+  
+
+         
+         
+         
+        
+        
+
