a982dd81ee788a868ac041e1dd758fa51b2b95c0
sindhunomi <sindhusrihari16@gmail.com>
2021-12-27 11:45:18 +0530

    program for finding the fibanocci series for first 10 terms

---
+++ javarelatedprograms/Fibanocci.java
@@ -0,0 +1,28 @@
+class Fibanocci
+ {
+    public static void main(String args[])
+     {
+          int f0 =0;
+          int f1 =1;
+          int f2 =f0+f1;
+           int f3 = f1+f2;
+           int f4 = f2+f3;
+           int f5 = f3+f4;
+           int f6= f5+f4;
+            int f7= f6+f5;
+           int f8 = f7+f6;
+            int f9 = f8+f7;
+            int f10 = f9+f8;
+    System.out.print(f0 + " ");   
+    System.out.print(f1 + " ");     
+    System.out.print(f2 + " ");   
+    System.out.print(f3 + " ");    
+   System.out.print(f4 + " ");   
+   System.out.print(f5 + " ");   
+   System.out.print(f6 + " ");   
+   System.out.print(f7 + " ");   
+   System.out.print(f8 + "  ");    
+  System.out.print(f9 + " ");   
+   System.out.print(f10 + " ");
+  }
+}     
