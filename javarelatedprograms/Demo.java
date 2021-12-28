f463a78c4055fdd2f8158dc7994b98132fc8db3e
sindhunomi <sindhusrihari16@gmail.com>
2021-12-28 13:09:02 +0530

    pattern program

---
+++ javarelatedprograms/Demo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Demo{
+public static void main(String args[]){
+   Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+       sc.close();
+        for(int i=1;i<=n;i++){
+              for(int j=i;j<=n;j++){
+                System.out.print(i);
+               }
+           System.out.println();
+        }
+     }
+  }    
+
+
+
+                 /*  5 ------>sample input
+ 	 11111
+	 2222
+	 333  ------>sample output
+	  44
+ 	  5     */
+              
