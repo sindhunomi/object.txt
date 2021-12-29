9e3c7264a615cababa75f37a875a214508fc4ea2
sindhunomi <sindhusrihari16@gmail.com>
2021-12-29 11:27:49 +0530

    overloading main method in java

---
+++ javarelatedprograms/Demo1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Demo1{
+       public static void main(String args[]){
+            System.out.println("main with  String args");
+             }
+                public static void main(String args)
+                  {
+                    System.out.println("main with  String");
+                  }
+                  public static void main()
+                    {
+                        System.out.println("main without args");
+                    }
+             }
