7adf0d5b93b3db04581be8a04fb440031deceb3a
sindhunomi <sindhusrihari16@gmail.com>
2022-01-19 19:12:37 +0530

    Printing the distinct values in array

---
+++ javarelatedprograms/Distinct.java
@@ -0,0 +1,12 @@
+  class Distinct {
+        public static void main(String args[]){
+              int arr [] = { 22,22,45,45 };
+                   for(int i = 0; i<arr.length-1; i++){
+                       if(arr[i]!=arr[i+1])
+                          System.out.println(arr[i]);
+                      }
+              //    System.out.println(arr[arr.length]);  
+               }
+        }
+          
+              
