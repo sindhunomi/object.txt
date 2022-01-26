13883a297285a78e28be3f0c8ec7ca590d84a506
sindhunomi <sindhusrihari16@gmail.com>
2022-01-26 19:51:46 +0530

    Reversing the  String

---
+++ javarelatedprograms/Day142.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.*;
+   class Day142{
+   public static void main(String args[]){
+      Scanner scan = new Scanner(System.in);
+      String str = scan.nextLine();
+      System.out.println(str.replaceAll("\s+"," ").trim());           // using regex triming the spaces 
+
+         }
+}
+                         /*   A line with     multiple spaces between    some words    --->Sample input and output
+                            A  line  with  multiple  spaces  between  some  words      */
+      
